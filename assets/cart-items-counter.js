32b473a156091caf1d9246bea364418c867eed3e
Mazen Khiami <sgaawc@gmail.com>
2022-12-15 16:07:57 +0100

    Template updatte

---
+++ assets/cart-items-counter.js
@@ -0,0 +1,69 @@
+(function defineCartItemCounterCustomElement () {
+
+  let btnText = 'Cart';
+  class CartItemsCounter extends HTMLElement {
+
+    elementsWithUpdatedCounter = listify('[data-cart-counter]');
+
+    constructor() {
+      super();
+  
+      this.addEventListener('click', this.onClick.bind(this));
+      this.addEventListener('cart-updated', this.onCartUpdate.bind(this));
+      this.addEventListener('cart-updated-with-count', this.onCartUpdateWithCount.bind(this));
+
+      this.innerHTML = btnText;
+    }
+
+    connectedCallback() {
+      this.onCartUpdate(null);
+    }
+
+    async onCartUpdate(e) {
+      try {
+      
+        // let payload = e?.detail;
+        let request = await fetch('/cart.js');
+        let { items } = (await request.json())||{};
+
+        // update 'cart-toggle' btn text
+        dispatchCustomEvent('update-product-form', items);
+
+        this.onCartUpdateWithCount({ detail: items.length });
+
+      } catch(e) {
+        logg('Cart Items Counter caught ', e);
+        this.innerHTML = btnText;
+      }
+    }
+
+    onCartUpdateWithCount(res) {
+      if (this.elementsWithUpdatedCounter?.length) {
+        this.elementsWithUpdatedCounter.forEach(a=> {
+          try {
+            if (!res.detail || res.detail <= 0) {
+              a.removeAttribute('data-cart-counter');
+            } else {
+              a.dataset.cartCounter = res.detail??null;
+            }
+          } catch(e) {logg('onCartUpdateWithCount caught ', e)}
+        });
+      }
+
+      if (res.detail > 0) {
+        this.innerHTML = `${btnText} (${res.detail})`; 
+
+      } else {
+        this.innerHTML = btnText;
+      }
+    }
+
+    onClick(e) {
+      location.href = '/cart';
+    }
+  }
+  
+  if (!customElements.get('cart-items-counter')) {
+    return customElements.define('cart-items-counter', CartItemsCounter);
+  };
+})();
--- assets/main.js
+++ assets/main.js
@@ -56,10 +56,199 @@
     });
   }
 
+  function newsletterState() {
+
+    let footer = query('footer');
+    let label = query('.footer-block--newsletter label');
+
+    if (label) label.addEventListener('click', ()=> {
+      footer.classList.toggle('open');
+    });
+
+  }
+
+  function onEscape() {
+
+    let elements = [
+      'footer',
+    ];
+
+    elements = elements.map(a=> query(a)).filter(a=> !!a);
+    if (elements?.length) elements.forEach(a=> {
+      a.classList?.remove('open');e
+    });
+  }
+
+  function bootstrapWistiaEmbedAPI() {
+    let _wq = [];
+    let pristine = true;
+    let wistiaRatio = whichWistiaRatio();
+    let autoplayedWistiaId = null;
+
+    _wq.push({
+      id: '_all',
+      options: {
+        videoFoam: false,
+        playerColor: '000000',
+        autoPlay: true,
+        muted: true,
+        controlsVisibleOnLoad: false,
+        playbar: false,
+        endVideoBehavior: 'loop',
+        playButton: false,
+        volumeControl: false,
+        fullscreenButton: false,
+        qualityControl: false,
+        playPauseNotifier: false,
+        playbackRateControl: false,
+      },
+      onReady: video => {
+        try {
+          
+          let hashedId = video.hashedId();
+          if (isNil(autoplayedWistiaId)) {
+            
+            let embed = video.getMediaElement().closest('.wistia_embed');
+            let isAutoplayed = embed.hasAttribute('data-autoplayed-wistia');
+            let sameRatio = embed.classList.contains(wistiaRatio);
+
+            if (isAutoplayed && sameRatio) {
+              autoplayedWistiaId = hashedId;
+              logg('autoplayed hashedId ', autoplayedWistiaId);
+            }
+
+          }
+          
+
+          video.bind('play', ()=> {
+
+            if (isSoundOn()) {
+              video.unmute();
+            }
+            
+            let videos = Wistia.api.all();
+            for (let i = 0; i < videos.length; i++) {
+              if (videos[i].hashedId() !== hashedId) {
+                videos[i].mute();
+
+                if (pristine && videos[i].hashedId() === autoplayedWistiaId) {
+                  continue;
+                }
+                videos[i].pause();
+              }
+            }
+          });
+
+          video.bind('crosstime', 2, ()=> {
+            
+            if (autoplayedWistiaId === hashedId) {
+              pristine = false;
+              dispatchCustomEvent('wsitia-control-sound', video);
+              triggerWindowResize();
+              logg('pristine/false');
+            }
+          });
+        
+          //setTimeout(()=> video.play(), 500); // force play
+          video.bind('end', ()=> video.play()); // force loop
+        } catch(e) { logg('bootstrapWistiaEmbedAPI caught ', e) }
+      }
+    });
+    return window._wq = _wq;
+  }
+
+  function switchIframeToWistiaEmbed() {
+    try {
+      let wistia = listify('iframe.embed-responsive-item');
+      if (!wistia?.length) return;
+      return wistia = wistia
+        .filter(a=> a.src.includes('wistia'))
+        .map(a=> {
+
+          // add desktop-resource automatically, only if there are multiple iframe
+          // backward-compatible
+          if (!a.className.includes('-resource') && listify('iframe.embed-responsive-item', a.parentElement)?.length > 1) {
+            a.classList.add('desktop-resource');
+          }
+
+          let [_, videoId] = a.src?.match(/http\s?.+\/(\S+)\?/)||[];
+          a.outerHTML = `<div class="wistia_embed wistia_async_${videoId} ${a.className}"></div>`
+
+          return a;
+
+        });
+    } catch(e) { logg('compatible wistia embed api - caught ', e ) }
+  }
+
+  function wistiaShowThumbnail() {
+
+    let p = [];
+    let wistias = listify('.wistia_embed');
+    if (!wistias?.length) return;
+    wistias.forEach(wistia=> {
+      let [_,hashedId] = wistia.className?.match(/\bwistia\_async\_(\S+)\b/i);
+      if (hashedId?.length) {
+        p.push(new Promise((resolve, reject)=> {
+          fetch(`https://fast.wistia.net/oembed?url=http://home.wistia.com/medias/${hashedId}?embedType=async_popover&videoWidth=${innerWidth}&videoHeight=${innerHeight}`)
+            .then(a=> a.json())
+            .then(a=> resolve({ 
+              wistia, hashedId, 
+              thumbnail: a.thumbnail_url,
+            }))
+            .catch(e=> reject(e));
+        }));
+      }
+    });
+
+    return Promise
+      .all(p)
+      .then(result=> {
+        if (result?.length) {
+          result.forEach(item=> {
+            try {
+              if (item.wistia?.parentElement) {
+                item.wistia.style.background = `transparent url(${item.thumbnail}) no-repeat center center/cover`;
+              }
+            } catch(e) {logg('wistia show thumbnail caught ', e)}
+          });
+        }
+      })
+      .catch(e=> logg('promise all caught ', e));
+
+  }
+
+  function responsiveWistia() {
+
+    let portraitWistia = listify('.portrait');
+    let landscapeWistia = listify('.landscape');
+
+    if (whichWistiaRatio() === 'portrait') {
+      landscapeWistia.forEach(r=> r.dataset.hidden = true);
+      portraitWistia.forEach(r=> r.dataset.hidden = false);
+    } else {
+      landscapeWistia.forEach(r=> r.dataset.hidden = false);
+      portraitWistia.forEach(r=> r.dataset.hidden = true);
+    }
+  }
+
   document.addEventListener('DOMContentLoaded', ()=> {
 
       mainnav();
+
+      newsletterState();
+    
+      switchIframeToWistiaEmbed();
+    
+      bootstrapWistiaEmbedAPI();
+
+      wistiaShowThumbnail();
+
+      document.addEventListener('keyup', e=> {
+        if (e.key === 'Escape') onEscape();
+      });
       
   });
+
+  window.addEventListener('resize', debounce(responsiveWistia.bind(undefined), 100, true));
 
 })();

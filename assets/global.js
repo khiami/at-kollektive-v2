0838ed8d6459df955141608a7f12b292c2f4ff44
Mazen Khiami <sgaawc@gmail.com>
2022-12-15 13:40:24 +0100

    Template updatte

--- assets/global.js
+++ assets/global.js
@@ -6,21 +6,21 @@ function getFocusableElements(container) {
   );
 }
 
-document.querySelectorAll('[id^="Details-"] summary').forEach((summary) => {
-  summary.setAttribute('role', 'button');
-  summary.setAttribute('aria-expanded', summary.parentNode.hasAttribute('open'));
+// document.querySelectorAll('[id^="Details-"] summary').forEach((summary) => {
+//   summary.setAttribute('role', 'button');
+//   summary.setAttribute('aria-expanded', summary.parentNode.hasAttribute('open'));
 
-  if(summary.nextElementSibling.getAttribute('id')) {
-    summary.setAttribute('aria-controls', summary.nextElementSibling.id);
-  }
+//   if(summary.nextElementSibling.getAttribute('id')) {
+//     summary.setAttribute('aria-controls', summary.nextElementSibling.id);
+//   }
 
-  summary.addEventListener('click', (event) => {
-    event.currentTarget.setAttribute('aria-expanded', !event.currentTarget.closest('details').hasAttribute('open'));
-  });
+//   summary.addEventListener('click', (event) => {
+//     event.currentTarget.setAttribute('aria-expanded', !event.currentTarget.closest('details').hasAttribute('open'));
+//   });
 
-  if (summary.closest('header-drawer')) return;
-  summary.parentElement.addEventListener('keyup', onKeyUpEscape);
-});
+//   if (summary.closest('header-drawer')) return;
+//   summary.parentElement.addEventListener('keyup', onKeyUpEscape);
+// });
 
 const trapFocusHandlers = {};
 
--- assets/main.js
+++ assets/main.js
@@ -1,28 +1,65 @@
 (()=> {
 
+  function mainnav() {
 
-    function mainnav() {
+    const header = query('header');
+    const injectableNav = query('.nav.n-level');
+    const itemsWithSubmenu = listify('header .list-menu__item');
 
-      let itemsWithSubmenu = listify('.list-menu__item');
+    function closeNavigation() {
 
-      itemsWithSubmenu.forEach(item=> {
-
-        item.addEventListener('click', ()=> {
-
-          // WIP
-          // item.
-        })
-        let submenu = 
-
-      })
+      itemsWithSubmenu.forEach(a=> {
+        a.parentElement?.classList.remove('open');
+        a.setAttribute('aria-expanded', false);
+      });
+      injectableNav.innerHTML = '';
 
     }
 
+    itemsWithSubmenu.forEach(item=> {
+      item.addEventListener('click', e=> {
 
-    document.addEventListener('DOMContentLoaded', ()=> {
+        e.stopPropagation();
+        if (!item.getAttribute('href')) e.preventDefault();
 
-        mainnav()
-        
+        let parent = item.parentElement;
+        let menu = query('.list-menu', parent);
+
+        itemsWithSubmenu.forEach(a=> {
+          if (a !== item) {
+            a.parentElement?.classList.remove('open');
+            a.setAttribute('aria-expanded', false);
+          }
+        });
+
+        if (parent.classList.contains('open')) {
+          parent.classList.remove('open');
+          injectableNav.innerHTML = null;
+        } else {
+          parent.classList.add('open');
+          if (menu) injectableNav.innerHTML = menu.outerHTML;
+        }
+
+        // set aria-expanded
+        item.setAttribute('aria-expanded', parent.classList.contains('open'));
+      });
     });
+
+    // auto-collapse header if clicked outside
+    document.addEventListener('click', e=> {
+      if (!elementBelongsTo(header, e.target)) closeNavigation();
+    });
+
+    // auto-collapse on [escape] clicked
+    document.addEventListener('keyup', e=> {
+      if (e.key === 'Escape') closeNavigation();
+    });
+  }
+
+  document.addEventListener('DOMContentLoaded', ()=> {
+
+      mainnav();
+      
+  });
 
 })();

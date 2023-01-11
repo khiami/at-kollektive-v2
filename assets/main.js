33ca3268b66e48c3eaf8aff51c16e342f10d8708
Mazen Khiami <sgaawc@gmail.com>
2023-01-11 16:07:25 +0100

    Updates

--- assets/main.js
+++ assets/main.js
@@ -649,6 +649,15 @@
     defineHeaderHeight();
     defineHeaderTop();
 
+    Shopify.analytics.publish('my_custom_event', {
+      foo: {
+        bar: '123',
+      },
+      abc: {
+        def: 'geh',
+      },
+    });
+
   });
     
   document.addEventListener('keyup', e=> e.key === 'Escape' &&  onEscape());
